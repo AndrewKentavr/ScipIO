10d2285fbe9262534fdc246530edaf6a8643b5ad
andrt <andr.toshachakov@gmail.com>
2021-08-21 23:56:49 +0300

    in short, something is moving forward

--- handlers/keyboards/inline/math_menu_inline.py
+++ handlers/keyboards/inline/math_menu_inline.py
@@ -3,7 +3,7 @@ import os
 from aiogram import types
 
 from data_b.dp_math import problem_translate_name
-from handlers.math.problems_category_math import callback_problems
+from handlers.math.problems_category_math import callback_problems, callback_problems_info
 
 
 def get_inline_math_url():
@@ -37,6 +37,29 @@ def get_inline_math_problems_category():
         buttons.append(
             types.InlineKeyboardButton(text=translated_name, callback_data=callback_problems.new(category=file_name)))
     keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+
+    return keyboard
+
+
+def get_inline_math_problems_category_info(info_problem):
+    buttons = []
+
+    for i in info_problem:
+        if 'Решение 1' in i:
+            buttons.append(types.InlineKeyboardButton(text='Решение 1', callback_data=callback_problems_info.new(category=file_name)))
+        elif 'Решение 2' in i:
+            buttons.append(types.InlineKeyboardButton(text='Решение 2', callback_data=callback_problems_info.new(category=file_name)))
+        elif 'Решение' in i:
+            buttons.append(types.InlineKeyboardButton(text='Решение', callback_data=callback_problems_info.new(category=file_name)))
+        elif 'Ответ' in i:
+            buttons.append(types.InlineKeyboardButton(text='Ответ', callback_data=callback_problems_info.new(category=file_name)))
+        elif 'Подсказка' in i:
+            buttons.append(types.InlineKeyboardButton(text='Подсказка', callback_data=callback_problems_info.new(category=file_name)))
+        elif 'Замечания' in i:
+            buttons.append(types.InlineKeyboardButton(text='Замечания', callback_data=callback_problems_info.new(category=file_name)))
+
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
     keyboard.add(*buttons)
 
     return keyboard
--- handlers/math/problems_category_math.py
+++ handlers/math/problems_category_math.py
@@ -10,6 +10,7 @@ from data_b.dp_math import problem_category_random
 from handlers.keyboards.inline import math_menu_inline
 
 callback_problems = CallbackData("problems", "category")
+callback_problems_info = CallbackData("values", "info")
 
 
 async def problems_category_start(message: types.Message):
@@ -19,8 +20,19 @@ async def problems_category_start(message: types.Message):
 
 async def problems_category_print(call: types.CallbackQuery, callback_data: dict):
     category = callback_data["category"]
-    cc = problem_category_random(category)
-    await call.message.answer(f'{cc}')
+    list_info_problem = problem_category_random(category)
+    title = list_info_problem[0]
+    href = list_info_problem[1]
+    subcategory = list_info_problem[2]
+    complexity, classes = list_info_problem[3], list_info_problem[4]
+    condition = list_info_problem[5]
+    info_problem = list_info_problem[6:]
+
+    await call.message.answer(
+        f'Название задания или его ID: {title}\nСсылка на задание: {href}\nПодкатегория: {subcategory}\n{complexity}, {classes}')
+    await call.message.answer(f'{condition}',
+                              reply_markup=math_menu_inline.get_inline_math_problems_category_info(info_problem))
+
     await call.answer()
 
 
@@ -29,6 +41,9 @@ def register_handlers_math_problem_category(dp: Dispatcher):
     dp.register_message_handler(problems_category_start, commands="problems_category")
     all_files_names = os.listdir(path="C:/Users/andrt/PycharmProjects/ConTia/data_b/json")
     all_file_names_list_not_json = [file_name_json.split('.json')[0] for file_name_json in all_files_names]
+
+    dp.register_callback_query_handler(problems_category_print,
+                                       callback_problems.filter(category=all_file_names_list_not_json), state='*')
 
     dp.register_callback_query_handler(problems_category_print,
                                        callback_problems.filter(category=all_file_names_list_not_json), state='*')
