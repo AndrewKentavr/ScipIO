79eeb5b5cd84301a520c39d47a36301a35e12079
AndrewKentavr <andr.toshachakov@gmail.com>
2022-01-10 06:53:13 +0300

    Переделал процесс тренировки mentallt_math

--- handlers/math/mentally_math.py
+++ handlers/math/mentally_math.py
@@ -14,7 +14,7 @@ async def equation_mentally_theory(message: types.Message):
         'Мы должны использовать: <b>Круглые Числа</b>\nОдин из самых распространённыхприёмов устного счёта'
         ' заключается в том, что любое число можно представить в виде суммы или разности чисел, одно или '
         'несколько из которых «круглое»', reply_markup=math_menu_inline.get_inline_math_url())
-    photo = InputFile("C:/Users/andrt/PycharmProjects/Scipio/data/math_1.jpg")
+    photo = InputFile("data/math_1.jpg")
     await message.answer_photo(photo=photo)
     await message.answer(
         '<b>Упростим умножение делением</b>\nПри устном счёте бывает удобнее оперировать делимым и делителем нежели '
@@ -31,7 +31,7 @@ async def equation_mentally_theory(message: types.Message):
         'квадрат, достаточно запомнить квадраты всех чисел от 1 до 25. Благо, квадраты до 10 мы уже знаем из таблицы '
         'умножения. Остальные квадраты можно посмотреть в нижеприведённой таблице:')
 
-    photo_2 = InputFile("C:/Users/andrt/PycharmProjects/Scipio/data/math_2.jpg")
+    photo_2 = InputFile("data/math_2.jpg")
     await message.answer_photo(photo=photo_2)
     await message.answer(
         '<b>Приём Рачинского</b> заключается в следующем. Для того чтобы найти квадрат любого двузначного числа, надо разность'
@@ -45,10 +45,10 @@ async def equation_mentally_theory(message: types.Message):
 async def equation_mentally_start(message: types.Message):
     await message.answer('Чтобы вызвать подсказку напишите /mell_theory')
     await message.answer('Вы готовы?', reply_markup=math_menu.get_keyboard_math_mentally_start())
-    await Equation.equation_mentally.set()
+    await Equation.equation_mentally_beginning.set()
 
 
-async def equation_mentally(message: types.Message, state: FSMContext):
+async def equation_mentally_beginning(message: types.Message, state: FSMContext):
     if message.text != 'Да':
         await message.answer('Вы написали что-то не то')
         return
@@ -57,69 +57,87 @@ async def equation_mentally(message: types.Message, state: FSMContext):
         # Генерирует пример ввида [equation, answer]
         equation = equation_generate()
 
-        user_data = await state.get_data()
-
         # ------  Создание user_data ---------
 
-        # При начале программы 'user_data' - пуста и создаются сделующие списки
-        if len(user_data) == 0:
-            await state.update_data(condition=[])
-            await state.update_data(answer=[])
-            await state.update_data(attempts=[])
-            user_data = await state.get_data()
+        # В начале программы 'user_data' - пуста и создаются сделующие списки
+        await state.update_data(condition=[])
+        await state.update_data(answer=[])
+        await state.update_data(attempts=[])
+
+        user_data = await state.get_data()
 
         # ------  Определение user_data -------
         # хранит все условия примеров
         conditions = user_data['condition']
         conditions.append(equation[0])
+        await state.update_data(condition=conditions)
+
         # хранит все ответы примеров
         answers = user_data['answer']
         answers.append(equation[1])
+        await state.update_data(answer=answers)
+
         # хранит всё количество попыток на ответы примеров
         attempt = user_data['attempts']
-        attempt.append(1)
-
-        # обновление user_data
-        await state.update_data(condition=conditions)
-        await state.update_data(answer=answers)
+        # Количество попыток мы создаём 0 - это сделанно для того, чтобы если пользователь завершит задание, не
+        # приступив к нему, то это не покажется в статистике, т.к попыток НОЛЬ
+        attempt.append(0)
         await state.update_data(attempts=attempt)
 
         await message.answer(f'Решите в уме:\n{equation[0]}')
-        await Equation.equation_mentally_answer.set()
+        await Equation.equation_mentally.set()
 
 
-async def equation_mentally_answer(message: types.Message, state: FSMContext):
-    msg = message.text
+async def equation_mentally(message: types.Message, state: FSMContext):
     try:
         msg = int(message.text)
     except ValueError:
         await message.answer(f'Неправильные знаки')
-
-    user_data = await state.get_data()
-    answer = user_data['answer'][-1]
-    conditions = user_data['condition'][-1]
-    attempt = user_data['attempts'][-1]
-
-    if msg != answer:
-
-        await message.answer('Неправильно, попробуйте ещё раз')
-        if attempt % 3 == 0:
-            await message.answer('Посмотрите ещё раз "подсказку":\n'
-                                 'Для этого нажмите или наберите /mell_theory')
-        await message.answer(f'Решите в уме:\n{conditions}')
-        # считает количество попыток и прибавляет
-        cc = attempt[-1] + 1
-        attempt[-1] = cc
-        await state.update_data(attempts=attempt)
-        return
     else:
-        await message.answer('Правильно, слидующее задание')
-        await Equation.equation_mentally.set()
+        user_data = await state.get_data()
+
+        answers = user_data['answer']
+        conditions = user_data['condition']
+        attempts = user_data['attempts']
+
+        # считает количество попыток и прибавляет
+        cc = int(attempts[-1]) + 1
+        attempts[-1] = cc
+        await state.update_data(attempts=attempts)
+
+        if msg != int(answers[-1]):
+
+            await message.answer('Неправильно, попробуйте ещё раз')
+            if int(attempts[-1]) % 3 == 0:
+                await message.answer('Посмотрите ещё раз "подсказку":\n'
+                                     'Для этого нажмите или наберите /mell_theory')
+            await message.answer(f'Решите в уме:\n{conditions[-1]}')
+            return
+        else:
+            await message.answer('Правильно, следующее задание')
+
+            # Генерирует пример ввида [equation, answer]
+            equation = equation_generate()
+
+            # ------  Добавляем примеры, ответы, попытки в  user_data -------
+
+            conditions.append(equation[0])
+            await state.update_data(condition=conditions)
+            answers.append(equation[1])
+            await state.update_data(answer=answers)
+            # Количество попыток мы создаём 0 - это сделанно для того, чтобы если пользователь завершит задание, не
+            # приступив к нему, то это не покажется в статистике, т.к попыток НОЛЬ
+            attempts.append(0)
+            await state.update_data(attempts=attempts)
+
+            await message.answer(f'Решите в уме:\n{equation[0]}')
+
+            await Equation.equation_mentally.set()
 
 
 class Equation(StatesGroup):
+    equation_mentally_beginning = State()
     equation_mentally = State()
-    equation_mentally_answer = State()
 
 
 def equation_generate():
@@ -149,5 +167,5 @@ def register_handlers_math_mentally(dp: Dispatcher):
 
     dp.register_message_handler(equation_mentally_start, Text(equals="Примеры для подсчёта в уме"))
     dp.register_message_handler(equation_mentally_start, commands="equation_mentally")
+    dp.register_message_handler(equation_mentally_beginning, state=Equation.equation_mentally_beginning)
     dp.register_message_handler(equation_mentally, state=Equation.equation_mentally)
-    dp.register_message_handler(equation_mentally_answer, state=Equation.equation_mentally_answer)
