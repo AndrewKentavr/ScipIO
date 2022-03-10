6ed573685ab23b342ac9fb4a62b3ba8cd44fc842
MrDoberman <ahmedshagbanov70@gmail.com>
2022-03-10 19:59:03 +0300

    Не ЧАЙНО убрал защиту от спама

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ async def main():
     """
     # Удаление последнего сообщения
     await bot.delete_webhook(drop_pending_updates=True)
+    middlewares.setup(dp)
 
     scheduler.start()
     timer_interval_func()
