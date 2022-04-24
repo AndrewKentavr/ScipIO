72547e82c35a2230090887e93337b93ea3304025
AndrewKentavr <andr.toshachakov@gmail.com>
2022-04-24 21:13:16 +0300

    fix timer (баг был в удалении пользователя при любой ошибке)

--- handlers/timer/timer_cycle.py
+++ handlers/timer/timer_cycle.py
@@ -26,37 +26,34 @@ async def time_cycle(dp):
         for i in time_results:
             user_id = i[0]
             tasks = i[1]
-            try:
-                """
-                Без создания state, на версии 2.16 aiogram выдаёт ошибку:
-                    AttributeError: 'NoneType' object has no attribute 'current_state'
-                    Поэтому надо создавать state вручную
-                """
-                state = dp.current_state(chat=user_id, user=user_id)
+            """
+            Без создания state, на версии 2.16 aiogram выдаёт ошибку:
+                AttributeError: 'NoneType' object has no attribute 'current_state'
+                Поэтому надо создавать state вручную
+            """
+            state = dp.current_state(chat=user_id, user=user_id)
 
-                await dp.bot.send_message(user_id, 'Ежедневное задание!',
-                                          reply_markup=types.ReplyKeyboardRemove())
+            await dp.bot.send_message(user_id, 'Ежедневное задание!',
+                                      reply_markup=types.ReplyKeyboardRemove())
 
-                # Идёт проверка какого типо tasks
-                if tasks == 'Карточки (Flashcards)':
-                    await dp.bot.send_message(user_id, 'Лайфхаки для работы с карточками /cards_info')
-                    await dp.bot.send_message(user_id, 'Вы готовы?',
-                                              reply_markup=flashcard_menu.get_keyboard_flashcard_training_start())
-                    await state.set_state(Flash_game.fls_game)
+            # Идёт проверка какого типо tasks
+            if tasks == 'Карточки (Flashcards)':
+                await dp.bot.send_message(user_id, 'Лайфхаки для работы с карточками /cards_info')
+                await dp.bot.send_message(user_id, 'Вы готовы?',
+                                          reply_markup=flashcard_menu.get_keyboard_flashcard_training_start())
+                await state.set_state(Flash_game.flc_game)
 
-                elif tasks == 'Математика в уме':
-                    await dp.bot.send_message(user_id, 'Чтобы вызвать подсказку напишите /mell_theory')
-                    await dp.bot.send_message(user_id, 'Вы готовы?',
-                                              reply_markup=math_menu.get_keyboard_math_mentally_start())
-                    await state.set_state(Equation.equation_mentally_beginning)
+            elif tasks == 'Математика в уме':
+                await dp.bot.send_message(user_id, 'Чтобы вызвать подсказку напишите /mell_theory')
+                await dp.bot.send_message(user_id, 'Вы готовы?',
+                                          reply_markup=math_menu.get_keyboard_math_mentally_start())
+                await state.set_state(Equation.equation_mentally_beginning)
 
-                elif tasks == 'Задачи по математике':
-                    await dp.bot.send_message(user_id, 'Выберете категорию заданий:',
-                                              reply_markup=math_menu_inline.get_inline_math_problems_category())
+            elif tasks == 'Задачи по математике':
+                await dp.bot.send_message(user_id, 'Выберете категорию заданий:',
+                                          reply_markup=math_menu_inline.get_inline_math_problems_category())
 
-                elif tasks == 'Задачи по логике':
-                    from handlers.keyboards.inline import logic_menu_inline
-                    await dp.bot.send_message(user_id, 'Выберете категорию заданий:',
-                                              reply_markup=logic_menu_inline.get_inline_logic_problems_category())
-            except:
-                del_user(user_id)
+            elif tasks == 'Задачи по логике':
+                from handlers.keyboards.inline import logic_menu_inline
+                await dp.bot.send_message(user_id, 'Выберете категорию заданий:',
+                                          reply_markup=logic_menu_inline.get_inline_logic_problems_category())
